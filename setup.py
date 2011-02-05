687793bd52a4a04c0995f7fe8e2503c4cd6253fe
Daniel Neuhäuser <dasdasich@gmail.com>
2011-02-05 22:08:09 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+from setuptools import setup
+
+
+setup(
+    name='Statemachine',
+    version='0.1dev',
+    url='http://github.com/DasIch/statemachine',
+    license='BSD',
+    author='Daniel Neuhäuser',
+    author_email='dasdasich@gmail.com',
+    description='A practical statemachine',
+    packages=['statemachine'],
+    zip_safe=False,
+    platforms='any'
+)
